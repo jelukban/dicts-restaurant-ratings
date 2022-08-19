51c93d8a6654ebc797c089fd432a00ae0395f452
Jerianne Lukban <jeriannelukban19@gmail.com>
2022-08-18 21:35:16 -0700

    Fixed indentation and alignment issues

--- ratings.py
+++ ratings.py
@@ -1,53 +1,46 @@
 """Restaurant rating lister."""
+def print_restaurant_ratings(filename):
+	restaurants = open(filename)
+	restaurant_and_rating = {}
 
-def restaurant_ratings(filename):
-  restaurants = open(filename)
-  restaurant_and_rating = {}
+	for line in restaurants:
+		restaurant, rating = line.rstrip().split(':')
+		restaurant_and_rating[restaurant] = rating
 
-  for line in restaurants:
-    restaurant, rating = line.rstrip().split(':')
+	sorted_restaurant_and_rating = sorted(restaurant_and_rating.items())
 
-    restaurant_and_rating[restaurant] = rating
+	for restaurant_tuple in sorted_restaurant_and_rating:
+		name, score = restaurant_tuple
+		print(f"{name} is rated at {score}.")
 
-  sorted_restaurant_and_rating = sorted(restaurant_and_rating.items())
-  for restaurant_tuple in sorted_restaurant_and_rating:
-    name, score = restaurant_tuple
-    print(f"{name} is rated at {score}.")
+	restaurants.close()
+	return restaurant_and_rating
 
-  restaurants.close()
-  return restaurant_and_rating
-
-# print (restaurant_ratings("scores.txt"))
 
 def adds_restaurant_ratings(filename):
-    while True:
+	while True:
+		restaurant = input("Enter a restaurant name: ")
+		rating = int(input("Enter a rating name: "))
 
-        restaurant = input("Enter a restaurant name: ")
-        rating = int(input("Enter a rating name: "))
+		if rating > 5 or rating < 1:
+			print("Pls enter 1 to 5")
+		else:
+			break
 
-        if rating > 5 or rating < 1:
-            print("Pls enter 1 to 5")
-
-        else:
-            break
-
-
-    restaurant_and_ratings = restaurant_ratings(filename)
-    restaurant_and_ratings[restaurant] = rating
-    print(f"{restaurant} is rated at {rating}")
-
-# print(adds_restaurant_ratings('scores.txt'))
+	restaurant_and_ratings = print_restaurant_ratings(filename)
+	restaurant_and_ratings[restaurant] = rating
+	print(f"{restaurant} is rated at {rating}")
 
 def user_choices():
-    choices = input ("Would you like to A) See all ratings B) Add restaurant or C) Quit ")
-    if choices == "A":
-       restaurant_ratings("scores.txt")
+	choices = input("Would you like to A) See all ratings B) Add restaurant or C) Quit ")
+	if choices == "A":
+		print_restaurant_ratings("scores.txt")
 
-    elif choices == "B":
-         adds_restaurant_ratings("scores.txt")
+	elif choices == "B":
+		adds_restaurant_ratings("scores.txt")
 
-    elif choices == "C":
-         return None
+	elif choices == "C":
+		return None
 
 print(user_choices())
 
